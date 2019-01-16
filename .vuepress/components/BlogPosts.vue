6d536a53480f637020122f9834329271d6123c93
181192 <181192@stud.hvl.no>
2019-01-16 22:53:49 +0100

    Update recent posts layout

--- .vuepress/components/BlogPosts.vue
+++ .vuepress/components/BlogPosts.vue
@@ -1,14 +1,26 @@
 <template>
-  <div>
+  <div class="blog-list">
     <div>
       <h2>Siste innlegg</h2>
     </div>
-    <div v-for="post in recentPosts" :key="post.frontmatter.date">
-      <router-link :to="post.path">
-        <ul>
-          <li>{{ post.title }} {{ new Date(post.frontmatter.date).toLocaleDateString() }}</li>
-        </ul>
-      </router-link>
+    <div class="post" v-for="post in recentPosts" :key="post.frontmatter.date">
+      <div class="box">
+        <article class="media">
+          <div class="media-left">
+            <figure class="image">📖</figure>
+          </div>
+          <div class="media-content">
+            <div class="content">
+              <p>
+                <router-link :to="post.path">
+                  <strong>{{ post.title }}</strong>
+                </router-link>
+                <small>{{ new Date(post.frontmatter.date).toLocaleDateString() }}</small>
+              </p>
+            </div>
+          </div>
+        </article>
+      </div>
     </div>
   </div>
 </template>
@@ -33,3 +45,50 @@ export default {
   }
 }
 </script>
+
+
+<style lang="stylus" scoped>
+@import url('https://fonts.googleapis.com/css?family=Muli:400,800')
+
+strong
+  font-weight 800
+
+p
+  line-height 1.2
+
+  + p
+    margin-top 5px
+
+.post
+  background-color #fff
+  color #373737
+  box-shadow 0 2px 3px rgba(10, 10, 10, 0.1), 0 0 0 1px rgba(10, 10, 10, 0.1)
+
+  + .post
+    margin-top 20px
+
+  .media
+    display flex
+
+  .media-left
+    // width 100px
+
+  figure
+    margin 16px
+    font-size 30px
+
+  .content
+    padding 0px 32px
+
+    > p
+      margin-top 16px
+
+  img
+    width 64px
+    margin-right 15px
+
+  small
+    font-size 14px
+    color #657786
+    display block
+</style>
