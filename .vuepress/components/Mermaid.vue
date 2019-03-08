b139582a153d84b6942d8009cefcd4a6bd8ec400
Peder <peder.wiig@gmail.com>
2019-03-08 18:03:06 +0100

    add support for mermaid markdown engine. this allows you to write flow charts and diagrams in markdownfiles. url: mermaid-support

---
+++ .vuepress/components/Mermaid.vue
@@ -0,0 +1,14 @@
+<template></template>
+
+<script>
+export default {
+  mounted() {
+    import("mermaid/dist/mermaid").then(m => {
+      m.initialize({
+        startOnLoad: true
+      });
+      m.init();
+    });
+  }
+};
+</script>
